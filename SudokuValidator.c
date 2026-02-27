ed34e0294d5b564f1f6f4311b176ed258f052da4
Qu3zada22 <139565119+Qu3zada22@users.noreply.github.com>
2026-02-26 19:57:23 -0600

    Validación subcuadrantes

--- SudokuValidator.c
+++ SudokuValidator.c
@@ -8,16 +8,20 @@
 #define SIZE 9
 
 int grid[SIZE][SIZE];
+
 int check_subgrid(int row_start, int col_start) {
 
     int seen[10] = {0};
 
     for(int i = 0; i < 3; i++) {
         for(int j = 0; j < 3; j++) {
+
             int num = grid[row_start + i][col_start + j];
+
             if(num < 1 || num > 9 || seen[num]) {
                 return 0;
             }
+
             seen[num] = 1;
         }
     }
@@ -42,6 +46,11 @@ int main(int argc, char *argv[]) {
     fstat(fd, &st);
 
     char *map = mmap(NULL, st.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
+    if(map == MAP_FAILED) {
+        perror("Error en mmap");
+        close(fd);
+        return 1;
+    }
 
     int index = 0;
     for(int i = 0; i < SIZE; i++) {
@@ -53,15 +62,28 @@ int main(int argc, char *argv[]) {
     munmap(map, st.st_size);
     close(fd);
 
+    printf("Archivo cargado correctamente.\n");
+
     for(int i = 0; i < 9; i += 3) {
-    for(int j = 0; j < 9; j += 3) {
-        if(!check_subgrid(i, j)) {
-            printf("Sudoku inválido (subcuadrantes)\n");
-            return 0;
+        for(int j = 0; j < 9; j += 3) {
+            if(!check_subgrid(i, j)) {
+                printf("Sudoku inválido (subcuadrantes)\n");
+                return 0;
+            }
         }
     }
-}
 
-printf("Subcuadrantes válidos.\n");
-return 0;
+    printf("Subcuadrantes válidos.\n");
+
+    pid_t pid = fork();
+
+    if(pid == 0) {
+        printf("Proceso hijo ejecutándose. PID: %d\n", getpid());
+        sleep(10);
+    } else {
+        printf("Proceso padre. PID hijo: %d\n", pid);
+        sleep(10);
+    }
+
+    return 0;
 }
